068093ef31dd6461a39f981eb962dc4eedd47217
Anwesh43 <anweshthecool0@gmail.com>
2017-04-23 16:19:06 +0530

    Adding HorizontalTextBarList class

---
+++ horizontaltextbar/src/main/java/com/anwesome/ui/horizontaltextbar/HorizontalTextBarList.java
@@ -0,0 +1,27 @@
+package com.anwesome.ui.horizontaltextbar;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 23/04/17.
+ */
+public class HorizontalTextBarList {
+    private Activity activity;
+    private boolean isShown = false;
+    private HorizontalTextBarView horizontalTextBarView;
+    public HorizontalTextBarList(Activity activity) {
+        this.activity = activity;
+        this.horizontalTextBarView = new HorizontalTextBarView(activity);
+    }
+    public void addTextBar(String title) {
+        if(!isShown) {
+            horizontalTextBarView.addTextBar(title);
+        }
+    }
+    public void show() {
+        if(!isShown) {
+            activity.setContentView(horizontalTextBarView);
+            isShown = true;
+        }
+    }
+}
