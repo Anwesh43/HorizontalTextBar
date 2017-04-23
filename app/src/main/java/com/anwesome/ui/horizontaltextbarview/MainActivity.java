c432aa0f8933890faa91d38104a80f52cc2a70f5
Anwesh43 <anweshthecool0@gmail.com>
2017-04-23 17:09:38 +0530

    Using OnExpandListener in app

--- app/src/main/java/com/anwesome/ui/horizontaltextbarview/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/horizontaltextbarview/MainActivity.java
@@ -2,28 +2,33 @@ package com.anwesome.ui.horizontaltextbarview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.anwesome.ui.horizontaltextbar.HorizontalTextBarList;
 import com.anwesome.ui.horizontaltextbar.HorizontalTextBarView;
+import com.anwesome.ui.horizontaltextbar.OnExpandListener;
 
 public class MainActivity extends AppCompatActivity {
+    private String titles[] = {"Hello","Joke","Cheer","Stick","Crossed"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         HorizontalTextBarList horizontalTextBarList = new HorizontalTextBarList(this);
-        horizontalTextBarList.addTextBar("Hello");
-        horizontalTextBarList.addTextBar("Joke");
-        horizontalTextBarList.addTextBar("Stir");
-        horizontalTextBarList.addTextBar("Cross");
-        horizontalTextBarList.addTextBar("Hello");
-        horizontalTextBarList.addTextBar("Joke");
-        horizontalTextBarList.addTextBar("Stir");
-        horizontalTextBarList.addTextBar("Cross");
-        horizontalTextBarList.addTextBar("Hello");
-        horizontalTextBarList.addTextBar("Joke");
-        horizontalTextBarList.addTextBar("Stir");
-        horizontalTextBarList.addTextBar("Cross");
+        for(int i=0;i<titles.length;i++) {
+            final String title = titles[i];
+            horizontalTextBarList.addTextBar(title, new OnExpandListener() {
+                @Override
+                public void onExpand() {
+                    Toast.makeText(MainActivity.this, title+" expanded", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onShrink() {
+                    Toast.makeText(MainActivity.this, title+" shrinked", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
         horizontalTextBarList.show();
     }
 }
