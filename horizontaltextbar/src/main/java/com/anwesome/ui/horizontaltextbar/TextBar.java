0a7418b1fbf4cc01756794f0036210b581002afd
Anwesh43 <anweshthecool0@gmail.com>
2017-04-23 13:07:19 +0530

    Adding TextBarButton

--- horizontaltextbar/src/main/java/com/anwesome/ui/horizontaltextbar/TextBar.java
+++ horizontaltextbar/src/main/java/com/anwesome/ui/horizontaltextbar/TextBar.java
@@ -1,14 +1,18 @@
 package com.anwesome.ui.horizontaltextbar;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 
 /**
  * Created by anweshmishra on 23/04/17.
  */
 public class TextBar {
     private String title;
-    private float x,y,w,h;
+    private float x,y,w,h,dir = 0,scale = 0,deg = 0;
+    private int barColor = Color.parseColor("#BDBDBD");
+    private TextBarButton textBarButton;
     public TextBar(String title) {
         this.title = title;
     }
@@ -17,12 +21,55 @@ public class TextBar {
         this.y = y;
         this.w = w;
         this.h = h;
+        textBarButton = new TextBarButton(w);
     }
     public void draw(Canvas canvas, Paint paint) {
-
+        canvas.save();
+        canvas.translate(x,y);
+        textBarButton.draw(canvas,paint);
+        canvas.restore();
     }
     public boolean handleTap(float x,float y) {
-        return false;
+        boolean condition =  textBarButton.handleTap(x,y);
+        if(condition) {
+            dir = deg == 0?1:-1;
+        }
+        return condition;
     }
-
+    public boolean stopped() {
+        return dir == 0;
+    }
+    public void update() {
+        deg+=18*dir;
+        scale+=0.2f*dir;
+        if(deg>=90 && deg<=0) {
+            dir = 0;
+        }
+    }
+    private class TextBarButton {
+        public float size,deg = 0;
+        public TextBarButton(float size) {
+            this.size = size;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            paint.setColor(barColor);
+            canvas.drawRect(new RectF(-size/2,-size/2,size/2,size/2),paint);
+            paint.setStrokeWidth(size/10);
+            paint.setColor(Color.BLACK);
+            canvas.save();
+            canvas.translate(size/2,size/2);
+            canvas.rotate(deg);
+            for(int i=0;i<2;i++) {
+                canvas.save();
+                canvas.rotate(90);
+                canvas.drawLine(-size/3,0,size/3,0,paint);
+                canvas.restore();
+            }
+            canvas.restore();
+        }
+        public boolean handleTap(float tapX,float tapY) {
+            boolean condition =  tapX>=x-size/2 && tapX<=x+size/2 && tapY>=x-size/2 && tapY>=y-size/2 && tapY<=y+size/2;
+            return condition;
+        }
+    }
 }
