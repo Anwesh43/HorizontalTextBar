177fc50b619d06888f450e308e991cca30afce2c
Anwesh43 <anweshthecool0@gmail.com>
2017-04-23 13:41:22 +0530

    Using AnimationController and drawing textbars

--- horizontaltextbar/src/main/java/com/anwesome/ui/horizontaltextbar/HorizontalTextBarView.java
+++ horizontaltextbar/src/main/java/com/anwesome/ui/horizontaltextbar/HorizontalTextBarView.java
@@ -2,6 +2,7 @@ package com.anwesome.ui.horizontaltextbar;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
 import java.util.ArrayList;
@@ -12,19 +13,41 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * Created by anweshmishra on 23/04/17.
  */
 public class HorizontalTextBarView extends View {
+    private float maxW = 0;
     private List<TextBar> textBars = new ArrayList<>();
+    private AnimationController animationController;
+    private int time = 0;
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private ConcurrentLinkedQueue<TextBar> bars = new ConcurrentLinkedQueue<>();
     public HorizontalTextBarView(Context context) {
         super(context);
     }
+    public void addTextBar(String title) {
+        textBars.add(new TextBar(title));
+    }
     public void onDraw(Canvas canvas) {
+        int w = canvas.getWidth(),h = canvas.getHeight()/2,gap = w/7,textBarH = h*3/5,x = 3*gap/2,y = h/5;
+        maxW = x;
+        if(time == 0) {
+            animationController = new AnimationController(this,textBars);
+            for(TextBar textBar:textBars) {
+                textBar.setDimension(x,y,w/7,textBarH);
+                x+=2*gap;
+            }
+            maxW+=2*gap+3*gap/2;
 
+        }
+        for(TextBar textBar:textBars) {
+            textBar.draw(canvas,paint);
+        }
+        time++;
+        if(animationController!=null) {
+            animationController.animate();
+        }
     }
     public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN) {
-            for(TextBar textBar:textBars) {
-
-            }
+        if(event.getAction() == MotionEvent.ACTION_DOWN && animationController!=null) {
+            animationController.startAnimating(event.getX(),event.getY());
         }
         return true;
     }
